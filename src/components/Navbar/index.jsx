533d9403f873ddb8df29d9d8074c83962ae47844
ourainbows <casasjuanfelipe@gmail.com>
2022-03-09 23:22:44 -0500

    Navbar component and its styles

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { AiFillGithub } from 'react-icons/ai';
+import { GrLinkedinOption } from 'react-icons/gr';
+import styles from "./styles.module.css"
 
 export const Navbar = () => {
-  return (
-    <div>index</div>
-  )
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.navNavigation}>
+                <div className={styles.navNavigationBrand}>Juan Casas<span> .</span></div>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Servicios</li>
+                    <li>Acerca de mi</li>
+                    <li>Proyectos</li>
+                </ul>
+            </div>
+            <div className={styles.navSocial}>
+                <a href="https://github.com/ourainbows">
+                    <AiFillGithub />
+                </a>
+                <a href="https://www.linkedin.com/in/jfcasas/">
+                    <GrLinkedinOption />
+                </a>
+            </div>
+        </nav>
+    )
 }
