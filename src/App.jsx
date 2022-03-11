9f37223a5794b030665fad3bb23149e17f9a8d74
ourainbows <casasjuanfelipe@gmail.com>
2022-03-11 01:48:58 -0500

    Component project  and its styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { About } from './components/About';
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
+import { Projects } from './components/Projects';
 import { Services } from './components/Services';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Home />
         <Services />
         <About />
+        <Projects/>
       </main>
     </>
   )
---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,26 @@
+import styles from "./styles.module.css"
+import ScrollContainer from 'react-indiana-drag-scroll'
+
+
+function Projects() {
+    return (
+        <section className={styles.sectionProjects}>
+            <h2 className={styles.title}>Proyectos</h2>
+            <div>
+                {/* mirar el end scroll mas la posicion en x */}
+                <ScrollContainer  className={styles.container}>
+                   <div className={styles.project}></div>
+                   <div className={styles.project}></div>
+                   <div className={styles.project}></div>
+                   <div className={styles.project}></div>
+                   <div className={styles.project}></div>
+                   <div className={styles.project}></div>
+                   <div className={styles.project}></div>
+                </ScrollContainer>
+            </div>
+
+        </section>
+    )
+}
+
+export { Projects }
