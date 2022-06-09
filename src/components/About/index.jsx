7598ceaff2c1f489f1ee9b7d12d07acdf0e995df
ourainbows <casasjuanfelipe@gmail.com>
2022-06-08 20:48:06 -0500

    updated about me: added angular and typescript in description

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -37,7 +37,7 @@ function About() {
                 <h2 className={styles.title}>Acerca de mi</h2>
                 <p className={styles.text}>
                     Mi nombre es Juan Felipe Casas, soy un desarrollador web enfocado al área del frontend.
-                    Tengo experiencia trabajando en proyectos reales con lenguajes de programación como <span>HTML, CSS, Javascript, React Js</span>, con el stack <span>MERN</span> y creando mockups con <span>Figma.</span><br/>
+                    Tengo experiencia trabajando en proyectos reales con lenguajes de programación como <span>HTML, CSS, Javascript, React Js, Angular, TypeScript</span>, con el stack <span>MERN</span> y creando mockups con <span>Figma.</span><br/>
                     Además tengo conocimientos de lado del backend en Python, y tengo conocimiento básico en bases de datos relacionales con mysql/mariadb y las bases de datos no relacionales con mongodb.<br/>
                     Para mi es muy importante la experiencia e interacción que va a tener el usuario con la aplicación y es por esto que también he aprendido algunos conceptos sobre <span>UI/UX</span> que me ayudan a tener un panorama y entendimiento más grande del desarrollo.<br/>
                     Este campo de la tecnología es un mundo por eso me mantengo en <span>constante aprendizaje</span>, actualmente estoy estudiando cursos de manera autodiacta en temas que complementen mis habilidades.<br/>
