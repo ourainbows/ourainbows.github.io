23add7b2d1953ce1135f4d6da24a45d7889767f7
ourainbows <casasjuanfelipe@gmail.com>
2022-06-08 20:29:46 -0500

    Updated about me

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -40,7 +40,7 @@ function About() {
                     Tengo experiencia trabajando en proyectos reales con lenguajes de programación como <span>HTML, CSS, Javascript, React Js</span>, con el stack <span>MERN</span> y creando mockups con <span>Figma.</span><br/>
                     Además tengo conocimientos de lado del backend en Python, y tengo conocimiento básico en bases de datos relacionales con mysql/mariadb y las bases de datos no relacionales con mongodb.<br/>
                     Para mi es muy importante la experiencia e interacción que va a tener el usuario con la aplicación y es por esto que también he aprendido algunos conceptos sobre <span>UI/UX</span> que me ayudan a tener un panorama y entendimiento más grande del desarrollo.<br/>
-                    Este campo de la tecnología es un mundo por eso me mantengo en <span>constante aprendizaje</span>, actualmente estoy estudiando en un bootcamp fullstack y de manera autónoma hago cursos de temas que complementen mis habilidades.<br/>
+                    Este campo de la tecnología es un mundo por eso me mantengo en <span>constante aprendizaje</span>, actualmente estoy estudiando de cursos manera de temas que complementen mis habilidades.<br/>
                     Mi meta es crear y contribuir en proyectos que tengan un cambio y mejoren la vida de las personas.<br/>
                     Me gustan los videojuegos, leer, el ajedrez y <i>"btw I use Arch"</i>
                 </p>
